e26d96e31cb354c7d1384ba027555695695f4c8e
MSI\banba <banban.n.namchan@gmail.com>
2021-06-29 16:16:11 +0900

    enemy.h、enemy.cppの内容をコピー

---
+++ GameProgramming/C_Practice10/01_Initializer/enemy.cpp
@@ -0,0 +1,35 @@
+
+#include"enemy.h"
+
+Enemy::Enemy()
+{
+	printf("コンストラクタ\n");
+	Name[0] = '\0';
+	Hp = 8;
+	Mp = 1;
+	Atk = 2;
+	Def = 1;
+}
+
+Enemy::~Enemy()
+{
+	printf("デストラクタ\n");
+}
+
+void Enemy::InitStatus(const char* enemyName)
+{
+	sprintf_s(Name, 32, "%s", enemyName);
+	Hp = 8;
+	Atk = 2;
+	Def = 1;
+}
+
+void Enemy::ShowStatus()
+{
+	printf("--- Enemy Status ---\n");
+	printf("Name : %s\n", Name);
+	printf("Hp:%d\n", Hp);
+	printf("Mp:%2d\n", Mp);
+	printf("Atk:%2d\n", Atk);
+	printf("Def:%2d\n", Def);
+}
