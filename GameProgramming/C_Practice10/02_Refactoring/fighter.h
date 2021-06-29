23e8774e292dfc21f0505dc5bce4df3355c79bb1
MSI\banba <banban.n.namchan@gmail.com>
2021-06-29 16:54:59 +0900

    fighter.h にプログラミング
    作業手順6.7.8.9.10を行った

--- GameProgramming/C_Practice10/02_Refactoring/fighter.h
+++ GameProgramming/C_Practice10/02_Refactoring/fighter.h
@@ -1,1 +1,24 @@
-#pragma once
+
+#ifndef FIGHTER_H
+#define FIGHTER_H
+
+class Fighter
+{
+public:
+	Fighter();
+	~Fighter();
+
+	char name[32];
+
+	int hp;
+	int str;
+	int mag;
+	int tec;
+	int spd;
+	int def;
+	int luc;
+	int mde;
+};
+
+#endif // !FIGHTER_H
+
