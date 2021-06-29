3b9306007b3c69c0afed60996923a69cb486344a
MSI\banba <banban.n.namchan@gmail.com>
2021-06-29 17:20:03 +0900

    作業手順_02_1終了

--- GameProgramming/C_Practice10/02_Refactoring/main.cpp
+++ GameProgramming/C_Practice10/02_Refactoring/main.cpp
@@ -1,9 +1,20 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include"fighter.h"
 
 int main()
 {
+	Fighter nanashi;
+
+	nanashi.hp = 8;
+	nanashi.str = 7;
+	nanashi.mag = 10;
+	nanashi.tec = 3;
+	nanashi.spd = 7;
+	nanashi.def = 2;
+	nanashi.luc = 6;
+	nanashi.mde = 9;
 
 	system("pause");
 	return 0;
