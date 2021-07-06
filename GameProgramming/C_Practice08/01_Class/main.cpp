fdf304e5f8d8e6f6de4aaeeaebba0ef76cafe079
MSI\banba <banban.n.namchan@gmail.com>
2021-07-06 17:43:37 +0900

    第8回＿作業01＿完了

---
+++ GameProgramming/C_Practice08/01_Class/main.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct StructHoge
+{
+	int value;
+};
+
+class ClassHoge
+{
+	int value;
+
+	void PrintHelloWorld();
+};
+
+void ClassHoge::PrintHelloWorld()
+{
+	printf("Hello World\n");
+}
+
+int main()
+{
+	StructHoge struct_hoge;
+	ClassHoge class_hoge;
+
+	struct_hoge.value = 1;
+
+	class_hoge.value = 10;
+	class_hoge.PrintHelloWorld();
+
+	system("pause");
+	return 0;
+}
